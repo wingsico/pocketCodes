fb876d43282d115b785be7c5e0686880e6607fdd
wingsico <wingsico@outlook.com>
2017-04-02 00:20:59 +0800

    滑动效果

--- js/prepareSlideShow.js
+++ js/prepareSlideShow.js
@@ -4,20 +4,27 @@
 function prepareSlideshow() {
   if(!document.getElementById) return false
   if(!document.getElementsByTagName) return false
-  if(!document.getElementById('preview')) return false
   if(!document.getElementById('linklist')) return false
-  var preview = document.getElementById('preview')
-  preview.style.position = 'absolute'
+  var slideShow = document.createElement('div')
+  slideShow.setAttribute('id','slideshow')
+  var preview = document.createElement('img')
+  preview.setAttribute('src','pngs/00.png')
+  preview.setAttribute('alt','动漫')
+  preview.setAttribute('id','preview')
+  preview.setAttribute('width','1200')
+  preview.setAttribute('height','300')
+  slideShow.appendChild(preview)
   var list = document.getElementById('linklist')
+  insertAfter(slideShow,list)
   var link = list.getElementsByTagName('a')
   link[0].onmouseover = function () {
-    moveElement("preview",-200,0,10)
+    moveElement("preview",-300,0,10)
   }
   link[1].onmouseover = function () {
-    moveElement("preview",-400,0,10)
+    moveElement("preview",-600,0,10)
   }
   link[2].onmouseover = function () {
-    moveElement("preview",-600,0,10)
+    moveElement("preview",-900,0,10)
   }
 }
 
